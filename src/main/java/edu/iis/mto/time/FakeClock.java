c9e592bbbbd3c75283273e903eddd35be72154e9
liberadzkih <liberadzkih@gmail.com>
2020-09-14 14:03:50 +0200

    Implemented FakeClock

--- src/main/java/edu/iis/mto/time/FakeClock.java
+++ src/main/java/edu/iis/mto/time/FakeClock.java
@@ -3,22 +3,29 @@ package edu.iis.mto.time;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public class FakeClock extends Clock {
 
-    public FakeClock() {
-        
+    private final ZoneId DEFAULT_TZONE = ZoneId.systemDefault();
+    private Instant instant;
+    private int hours;
+
+    public FakeClock(int hours) {
+        this.hours = hours;
+        this.instant = Instant.now();
     }
 
     @Override public ZoneId getZone() {
-        return null;
+        return DEFAULT_TZONE;
     }
 
     @Override public Clock withZone(ZoneId zone) {
-        return null;
+        return this;
     }
 
     @Override public Instant instant() {
-        return null;
+        instant = instant.plus(hours, ChronoUnit.HOURS);
+        return instant;
     }
 }
