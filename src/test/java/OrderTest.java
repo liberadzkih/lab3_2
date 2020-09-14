03b1a82b011ad3fc66cda4d03989aa83b46e6d86
liberadzkih <liberadzkih@gmail.com>
2020-09-14 14:04:45 +0200

    Added first test (OrderExpiredException test)

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,3 +1,24 @@
+import edu.iis.mto.time.FakeClock;
+import edu.iis.mto.time.Order;
+import edu.iis.mto.time.OrderExpiredException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.Clock;
+
 public class OrderTest {
+
+    Order order;
+    Clock fakeClock;
+
+    @Test
+    public void callConfirmMethod_OrderExpiredExceptionTest() {
+        fakeClock = new FakeClock(30);
+        order = new Order(fakeClock);
+        order.submit();
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
+    }
 
 }
